983414178f3f82fdcc75defaca9bbb0a6b76c2b6
Daniel Ilievski <ilievskiot@hotmail.com>
2022-06-11 21:37:57 +0200

    Create register view and functionallity

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class RegisterController extends Controller
 {
@@ -50,9 +51,13 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'name'                  => ['required', 'string', 'max:50'],
+            'last_name'             => ['required', 'string', 'max:50'],
+            'phone_number'          => [Rule::phone()->country(['MK'])],
+            'email'                 => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password'              => ['required', 'string', 'min:8', 'confirmed'],
+            'password_confirmation' => ['required', 'string', 'min:8'],
+            'agree_term'            => ['required'],
         ]);
     }
 
@@ -63,11 +68,13 @@ class RegisterController extends Controller
      * @return \App\Models\User
      */
     protected function create(array $data)
-    {
+    {   
         return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+            'name'              => $data['name'],
+            'last_name'         => $data['last_name'],
+            'phone_number'      => $data['phone_number'],
+            'email'             => $data['email'],
+            'password'          => Hash::make($data['password']),
         ]);
     }
 }
